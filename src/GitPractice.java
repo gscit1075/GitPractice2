76cf5226123033ceefec94782fca055fbfa261d5
gscit1075 <39850291+gscit1075@users.noreply.github.com>
2018-06-03 09:41:08 +0530

    Update GitPractice.java

--- src/GitPractice.java
+++ src/GitPractice.java
@@ -2,8 +2,8 @@
 public class GitPractice {
 
 	public static void main(String[] args) {
-	System.out.println("through the eclips update message");
-
+	System.out.println("through the eclips update message ");
+	System.out.println("From the server eclips update message ");
 	}
 
 }
